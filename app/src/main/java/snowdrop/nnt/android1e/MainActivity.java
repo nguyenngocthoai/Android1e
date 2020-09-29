88ddc2183dee6969e2400a94fdebde58d56a31e3
ThoaiVocie <thoainguyen603@gmail.com>
2020-09-29 17:58:35 +0700

    Android1e project first commit

---
+++ app/src/main/java/snowdrop/nnt/android1e/MainActivity.java
@@ -0,0 +1,14 @@
+package snowdrop.nnt.android1e;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
